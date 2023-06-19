00e3c9715e1a467fbb52122797257c451186aea2
roxanafrunza <frunza_roxana@yahoo.com>
2023-06-19 20:27:36 +0300

    feat: created races page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
 import DriversPage from '@/views/DriversPage.vue'
+import RacesPage from '@/views/RacesPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/drivers',
       name: 'drivers',
       component: DriversPage,
+    },
+    {
+      path: '/races',
+      name: 'races',
+      component: RacesPage,
     }
   ]
 })
