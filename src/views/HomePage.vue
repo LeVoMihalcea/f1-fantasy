6a4c8e792800c148ef38b90636dbec908d684285
Leo Mihalcea <leonard.mihalcea@cloudflight.io>
2023-06-19 18:32:50 +0300

    feat: Removed Bloat from Project

---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+    <HelloWorld :msg="greetingFromHomePage"></HelloWorld>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
+import { ref } from 'vue';
+
+const greetingFromHomePage = ref('Hello from Home Page');
+
+</script>
